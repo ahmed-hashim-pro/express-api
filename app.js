316d713c6521433e5de133824406f2a714ebf014
Hashim <medoalkhateeb@ymail.com>
2023-01-18 22:25:04 +0200

    4

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/products', boardsRouter);
+app.use('/boards', boardsRouter);
 
 
 module.exports = app;
